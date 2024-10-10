cc5686af835141d3c5020685be4dbdd935dfcce0
Minhnp0903 <minh.nguyen.e@ctmcorp.com.vn>
2024-10-10 17:28:40 +0700

    modify: run-odoo --dev now performs hot reload backend_views: fixed bug with tag model

--- run-odoo.bat
+++ run-odoo.bat
@@ -5,13 +5,27 @@ set ADDONS_PATH="addons,custom-addons"
 set DATABASE=ctnews
 
 REM Base command without the update argument
-set CMD=python odoo-bin -r %DB_USER% -w %DB_PASSWORD% --addons-path=%ADDONS_PATH% -d %DATABASE% --dev=reload
+set CMD=python odoo-bin -r %DB_USER% -w %DB_PASSWORD% --addons-path=%ADDONS_PATH% -d %DATABASE%
+
+REM Initialize the full command
+set FULL_CMD=%CMD%
+
+REM Check if --dev flag is present
+for %%A in (%*) do (
+    if "%%A"=="--dev" (
+        set FULL_CMD=%FULL_CMD% --dev=reload
+    )
+)
 
 REM Check if an argument was provided (-u)
 if "%1"=="" (
-    echo Running without the -u argument
-    %CMD%
+    REM If no argument is given, just run the command
+    set FINAL_CMD=%FULL_CMD%
 ) else (
-    echo Running with the -u argument
-    %CMD% %*
+    REM If an argument is given, add it to the command
+    set FINAL_CMD=%FULL_CMD% %*
 )
+
+REM Echo the final command to be executed
+echo Executing: %FINAL_CMD%
+%FINAL_CMD%
